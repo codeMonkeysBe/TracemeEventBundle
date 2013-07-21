aad975881e7fe44045549068e7c1c6e641e582bb
Niki Van Cleemput <niki@codemonkeys.be>
2013-07-21 17:21:58 +0200

    db store

--- Controller/EventController.php
+++ Controller/EventController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use Symfony\Component\HttpFoundation\Response;
+use CodeMonkeys\IntelliTrail\Bundle\EventBundle\Entity\Event;
 
 
 class EventController extends Controller
@@ -20,6 +21,7 @@ class EventController extends Controller
 
         $request        = $this->get('request');
         $cgpsService    = $this->get('cgps_service');
+        $extraData      = ''; 
 
         $cgps =  $cgpsService->Cgps();
         $cgps->ClearResponseActionMembers();
@@ -40,10 +42,46 @@ class EventController extends Controller
 
         }
 
-        $pdp = $cgps->GetDataPartCount();
+        $pdp = $this->storeEventFromCgpsData( $cgps, $extraData );
 
         return new Response( $cgps->BuildResponseHTTP( $pdp ), 200, array() );
 
+
+    }
+
+    protected function storeEventFromCgpsData( $cgps, $extraData )
+    {
+
+        $em = $this->get('doctrine')->getManager();
+        
+        $processedDataParts = 0;
+
+        for( ;$processedDataParts < $cgps->GetDataPartCount(); $processedDataParts++ ){
+
+            if(!$pcGPS->SelectDataPart($ProcessedDataParts) || !$pcGPS->IsValid())
+            {
+                continue;
+            }
+
+            $event = new Event();
+
+            $event->setDate( new DateTime( '@' . $cgps->GetUtcTime() ) );
+            $event->setImei( $cgps->GetImei() );
+            $event->setSwitch( $cgps->GetSwitch() );
+            $event->setEventid( $cgps->CanGetEventID() ? $cgps->GetEventID() : 0 );
+            $event->setLatitude( $cgps->GetLatitudeSmall() );
+            $event->setLongitude( $cgps->GetLongitudeSmall() );
+            $event->setIo( $cgps->GetIO() );
+            $event->setData( $cgps->GetBinaryData() );
+            $event->setExtra( $extraData );
+
+            $em->persist( $event );
+
+        }
+
+        $em->flush();
+
+        return $processedDataParts;
 
     }
 
